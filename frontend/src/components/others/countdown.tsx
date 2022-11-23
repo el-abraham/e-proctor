2c9012402316f1d6297aa47e334f67d8ec220d43
Emanuel1009 <73428926+Emanuel1009@users.noreply.github.com>
2022-11-23 16:28:58 +0700

    Update Countdown
    Update Countdown

--- frontend/src/components/others/countdown.tsx
+++ frontend/src/components/others/countdown.tsx
@@ -1,7 +1,68 @@
-export default function Countdown({}) {
+import React from "react";
+
+type Countdown = {
+  hours: number;
+  minutes: number;
+  seconds: number;
+};
+
+const CountDownTimer = ({ hours = 0, minutes = 0, seconds = 0 }: Countdown) => {
+  const [time, setTime] = React.useState<Countdown>({
+    hours,
+    minutes,
+    seconds,
+  });
+
+  const tick = () => {
+    if (time.hours === 0 && time.minutes === 0 && time.seconds === 0) reset();
+    else if (time.hours && time.seconds === 0) {
+      setTime({ hours: time.hours - 1, minutes: 59, seconds: 59 });
+    } else if (time.seconds === 0) {
+      setTime({ hours: time.hours, minutes: time.minutes - 1, seconds: 59 });
+    } else {
+      setTime({
+        hours: time.hours,
+        minutes: time.minutes,
+        seconds: time.seconds - 1,
+      });
+    }
+  };
+
+  const reset = () =>
+    setTime({
+      hours: time.hours,
+      minutes: time.minutes,
+      seconds: time.seconds,
+    });
+
+  React.useEffect(() => {
+    const timerId = setInterval(() => tick(), 1000);
+    return () => clearInterval(timerId);
+  });
+
   return (
     <div>
-      <h1 className="font-['Poppins'] font-bold text-[20px]">Hallo...</h1>
+      <p>{`${time.hours.toString().padStart(2, "0")}:${time.minutes
+        .toString()
+        .padStart(2, "0")}:${time.seconds.toString().padStart(2, "0")}`}</p>
     </div>
   );
-}
+};
+
+export default CountDownTimer;
+
+// type Props = {
+//   seconds?: BigInt;
+//   children?: React.ReactNode;
+// };
+
+// const Countdown = ({ seconds }: Props) => {
+//   const [countdown, setCoundown] = useState(seconds);
+//   const timerID = useRef;
+
+//   useEffect(() => {
+//     timerID.current = setInterval(() => {
+//       setCoundown((prev) => prev - 1);
+//     }, 1000);
+//     return () => clearInterval(timerID);
+//   }, []);
