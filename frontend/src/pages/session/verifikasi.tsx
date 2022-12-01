e4cf7c5fb313e3778ae13525626bff0b123b3e2d
Emanuel1009 <73428926+Emanuel1009@users.noreply.github.com>
2022-12-01 16:48:56 +0700

    Update
    Siswa:
    Input Gabung ujian + Cari ujian
    Webcamera

--- frontend/src/pages/session/verifikasi.tsx
+++ frontend/src/pages/session/verifikasi.tsx
@@ -1,60 +1,58 @@
 import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
 import { useRef } from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Webcam from "react-webcam";
 import { useState } from "react";
 import Button from "../../components/forms/Button";
 
+const videoConstraints = {
+  width: 500,
+  height: 500,
+  facingMode: "user",
+};
+
 export default function VerifikasiKamera() {
-  // https://dev.to/0xkoji/use-webcam-with-react-easily-397f
-  // https://www.youtube.com/watch?v=0HJ1cMBkWJ4&ab_channel=SyedZano
-  //   const img = useState(console.log(webcam.current?.getScreenshot()));
-
-  //   const [image, setImage] = useState<string>();
-
-  //   const capture = useCallback(() => {
-  //     const pictureSrc = webcam.current.getScreenshot();
-  //     setImage(pictureSrc);
-  //   });
+  const navigate = useNavigate();
 
   const webcam = useRef<Webcam>(null);
-
   const [img, setImg] = useState("");
-
-  //   function onClick(e: any) {
-  //     console.log
-  //     setImg(webcam.current?.getScreenshot());
-  //   }
-
   const showImage = () => {
     setImg(webcam.current!.getScreenshot()!.toString());
+  };
+
+  const PreExam = () => {
+    navigate(-1);
   };
 
   return (
     <div className="bg-[#EFF0F3] min-h-screen py-[30px] px-[50px]">
       <div className="alert alert-warning shadow-lg max-w-xl mx-auto mb-5">
         <div className="mx-auto">
-          <ExclamationTriangleIcon className="w-9 h-9" />
+          <ExclamationTriangleIcon className="w-8 h-8" />
           <span>
             Arahkan wajah anda ke dalam frame kamera untuk verifikasi!
           </span>
         </div>
       </div>
-
-      <Webcam
-        audio={false}
-        ref={webcam}
-        className="w-[500px] rounded-lg mx-auto border-2 border-black"
-      />
-
-      <img src={img}></img>
+      <div className="flex gap-x-5 w-fit mx-auto">
+        <Webcam
+          audio={false}
+          ref={webcam}
+          mirrored
+          screenshotFormat="image/jpeg"
+          videoConstraints={videoConstraints}
+          className="rounded-lg mx-auto border-2 border-black"
+        />
+        <img
+          src={img}
+          className="w-[500px] rounded-lg mx-auto border-2 border-black border-opacity-60 border-dashed"
+        ></img>
+      </div>
 
       <div className="flex mt-5 justify-center">
-        <Link to={"#"}>
-          <Button className="mr-5 bg-neutral">
-            <p>Kembali</p>
-          </Button>
-        </Link>
+        <Button className="mr-5 bg-neutral" onClick={PreExam}>
+          <p>Kembali</p>
+        </Button>
         <button onClick={showImage} className="btn btn-primary">
           Tangkap Layar
         </button>
